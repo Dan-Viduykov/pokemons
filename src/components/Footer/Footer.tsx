9f47f83c6759536f4b9d37ec8c48fe1234b22d89
Danya_Vid <danilviduykov@gmail.com>
2022-07-23 14:46:33 +0500

    fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import Container from "../Container";
+import Title from "../Title";
 import styles from "./Footer.module.scss";
 
 const Footer: FC = () => {
     return (
         <footer className={styles.footer}>
             <Container className={styles.footer__container}>
-                Footer
+                <Title>Footer</Title>
             </Container>
         </footer>
     )
