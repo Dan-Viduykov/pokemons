159dd037f9325d55d1e3183b6d117d9c4dbe1c36
Danya_Vid <danilviduykov@gmail.com>
2022-07-25 10:44:59 +0500

    some fix

--- src/components/screens/PokemonCard/SectionImg/SectionImg.tsx
+++ src/components/screens/PokemonCard/SectionImg/SectionImg.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../../../hooks/redux";
 import { IPokemon } from "../../../../models/pokemon";
