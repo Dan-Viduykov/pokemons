7482be6bf0c076a82630e7ab41a24bd70dc91273
Danya_Vid <danilviduykov@gmail.com>
2022-07-25 10:44:24 +0500

    some fix

--- src/store/pokemon.slice.ts
+++ src/store/pokemon.slice.ts
@@ -10,9 +10,11 @@ export interface IFav {
     id: number;
 }
 
+const FAV_KEY = 'FAV_KEY'
+
 const initialState: AppState = {
     currentPokemon: '',
-    favorites: []
+    favorites: JSON.parse(localStorage.getItem(FAV_KEY) ?? '[]')
 }
 
 export const AppSlice = createSlice({
@@ -23,14 +25,13 @@ export const AppSlice = createSlice({
             state.currentPokemon = action.payload;
         },
         addFavorite: (state, action: PayloadAction<IFav>) => {
-            state.favorites.push(action.payload)
+            state.favorites.push(action.payload);
+            localStorage.setItem(FAV_KEY, JSON.stringify(state.favorites))
         },
         removeFavorite: (state, action: PayloadAction<string>) => {
-            const fav = state.favorites
-            const idx = fav.findIndex(fav => fav.name === action.payload);
-            
-            state.favorites = [...fav.slice(0, idx), ...fav.slice(idx + 1)]
-        }
+            state.favorites = state.favorites.filter(fav => fav.name !== action.payload)
+            localStorage.setItem(FAV_KEY, JSON.stringify(state.favorites))
+        },
     }
 })
 
